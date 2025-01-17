e270c27c947ca8c5755aecdf587ff988ee5e8072
Varun Krishna. P <Varunkrishna.dbz@gmail.com>
2025-01-17 06:19:38 +0530

    Adding the updated pom.xml

---
+++ log-demo/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+    private final static String CONFIG_LOCATION  = "beans.xml";
+    public static void main(String[] args) {
+
+        log.info("Guess the number");
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
+
+        int number = numberGenerator.next();
+
+        log.info("number = {}", number);
+
+        context.close();
+    }
+}
